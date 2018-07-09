6c1404ebe40b139cd50e3ab5faa029e11fc12481
Daniel Rojas <danired18@gmail.com>
2018-07-09 14:50:01 -0400

    Fixed Errors

--- src/main/java/ucab/ihc/schedulemaker/service/ScheduleService.java
+++ src/main/java/ucab/ihc/schedulemaker/service/ScheduleService.java
@@ -111,49 +111,103 @@ public class ScheduleService {
         });
         subjectList.sort(new SortByNumberOfSections());
     }
+
+    private boolean checkThreeHours(String string, String string1, String string2, String string3){
+        String aux1 = string.split(":")[0];
+        String aux2 = string.split(":")[0];
+        String aux3 = string.split(":")[0];
+        String aux4 = string.split(":")[0];
+        int firstHourSect1 =  Integer.parseInt(aux1);
+        int secondHourSect1 = Integer.parseInt(aux2);
+        int firstHourSect2 = Integer.parseInt(aux3);
+        int secondHourSect2 = Integer.parseInt(aux4);
+        if((secondHourSect1 - firstHourSect1 > 1) && (secondHourSect2 - firstHourSect2 <= 1)){
+            int middleHour = secondHourSect1 - firstHourSect1;
+            if(middleHour == firstHourSect2)
+                return true;
+            if(middleHour == secondHourSect2)
+                return true;
+        }
+        if((secondHourSect1 - firstHourSect1 <= 1) && (secondHourSect2 - firstHourSect2 > 1)){
+            int middleHour = secondHourSect2 - firstHourSect2;
+            if(middleHour == firstHourSect1)
+                return true;
+            if(middleHour == secondHourSect1)
+                return true;
+        }
+        if((secondHourSect1 - firstHourSect1 > 1) && (secondHourSect2 - firstHourSect2 > 1)){
+            int middleHour1 = secondHourSect1 - firstHourSect1;
+            int middleHour2 = secondHourSect2 - firstHourSect2;
+            if(middleHour1 == firstHourSect2)
+                return true;
+            if(middleHour1 == secondHourSect2)
+                return true;
+            if(middleHour2 == firstHourSect1)
+                return true;
+            if(middleHour2 == secondHourSect1)
+                return true;
+            if(middleHour1 == middleHour2)
+                return true;
+        }
+        return false;
+    }
     private boolean checkConflict(Section[] combination, Section section, int length){
 
         for(int i = 0; i < length; i++){
             if((combination[i].getMon() != null) && (section.getMon() != null)) {
                 if (combination[i].getMon().getFirstHour().equals(section.getMon().getFirstHour()) ||
                         combination[i].getMon().getFirstHour().equals(section.getMon().getSecondHour()) ||
-                        combination[i].getMon().getSecondHour().equals(section.getMon().getFirstHour()))
+                        combination[i].getMon().getSecondHour().equals(section.getMon().getFirstHour()) ||
+                        checkThreeHours(combination[i].getMon().getFirstHour(), combination[i].getMon().getSecondHour(),
+                                section.getMon().getFirstHour(),section.getMon().getSecondHour()) )
                     return true;
             }
             if((combination[i].getThu() != null) && (section.getThu() != null)) {
                 if (combination[i].getThu().getFirstHour().equals(section.getThu().getFirstHour()) ||
                         combination[i].getThu().getFirstHour().equals(section.getThu().getSecondHour()) ||
-                        combination[i].getThu().getSecondHour().equals(section.getThu().getFirstHour()))
+                        combination[i].getThu().getSecondHour().equals(section.getThu().getFirstHour()) ||
+                        checkThreeHours(combination[i].getThu().getFirstHour(), combination[i].getThu().getSecondHour(),
+                                section.getThu().getFirstHour(),section.getThu().getSecondHour()))
                     return true;
             }
             if((combination[i].getWed() != null) && (section.getWed() != null)) {
                 if (combination[i].getWed().getFirstHour().equals(section.getWed().getFirstHour()) ||
                         combination[i].getWed().getFirstHour().equals(section.getWed().getSecondHour()) ||
-                        combination[i].getWed().getSecondHour().equals(section.getWed().getFirstHour()))
+                        combination[i].getWed().getSecondHour().equals(section.getWed().getFirstHour()) ||
+                        checkThreeHours(combination[i].getWed().getFirstHour(), combination[i].getWed().getSecondHour(),
+                                section.getWed().getFirstHour(),section.getWed().getSecondHour()))
                     return true;
             }
             if((combination[i].getTue() != null) && (section.getTue() != null)) {
                 if (combination[i].getTue().getFirstHour().equals(section.getTue().getFirstHour()) ||
                         combination[i].getTue().getFirstHour().equals(section.getTue().getSecondHour()) ||
-                        combination[i].getTue().getSecondHour().equals(section.getTue().getFirstHour()))
+                        combination[i].getTue().getSecondHour().equals(section.getTue().getFirstHour()) ||
+                        checkThreeHours(combination[i].getTue().getFirstHour(), combination[i].getTue().getSecondHour(),
+                                section.getTue().getFirstHour(),section.getTue().getSecondHour()))
                     return true;
             }
             if((combination[i].getFri() != null) && (section.getFri() != null)) {
                 if (combination[i].getFri().getFirstHour().equals(section.getFri().getFirstHour()) ||
                         combination[i].getFri().getFirstHour().equals(section.getFri().getSecondHour()) ||
-                        combination[i].getFri().getSecondHour().equals(section.getFri().getFirstHour()))
+                        combination[i].getFri().getSecondHour().equals(section.getFri().getFirstHour()) ||
+                        checkThreeHours(combination[i].getFri().getFirstHour(), combination[i].getFri().getSecondHour(),
+                                section.getFri().getFirstHour(),section.getFri().getSecondHour()))
                     return true;
             }
             if((combination[i].getSat() != null) && (section.getSat() != null)) {
                 if (combination[i].getSat().getFirstHour().equals(section.getSat().getFirstHour()) ||
                         combination[i].getSat().getFirstHour().equals(section.getSat().getSecondHour()) ||
-                        combination[i].getSat().getSecondHour().equals(section.getSat().getFirstHour()))
+                        combination[i].getSat().getSecondHour().equals(section.getSat().getFirstHour()) ||
+                        checkThreeHours(combination[i].getSat().getFirstHour(), combination[i].getSat().getSecondHour(),
+                                section.getSat().getFirstHour(),section.getSat().getSecondHour()))
                     return true;
             }
             if((combination[i].getSun() != null) && (section.getSun() != null)) {
                 if (combination[i].getSun().getFirstHour().equals(section.getSun().getFirstHour()) ||
                         combination[i].getSun().getFirstHour().equals(section.getSun().getSecondHour()) ||
-                        combination[i].getSun().getSecondHour().equals(section.getSun().getFirstHour()))
+                        combination[i].getSun().getSecondHour().equals(section.getSun().getFirstHour()) ||
+                        checkThreeHours(combination[i].getSun().getFirstHour(), combination[i].getSun().getSecondHour(),
+                                section.getSun().getFirstHour(),section.getSun().getSecondHour()))
                     return true;
             }
             if((section.getMon() == null) && (section.getTue() == null) && (section.getWed() == null) && (section.getThu() == null)
